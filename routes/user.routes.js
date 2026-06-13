b42253a86019ac38dbf0a1b759822fe28b7e991b
Md Tasin Tahmid <md.tasin.tahmid1998@gmail.com>
2026-06-13 23:12:39 +0600

    Add signup route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,13 @@
 import express from "express";
+import db from "../db";
+import { usersTable } from "../models/user.model";
 
 const router = express.Router();
 
+router.post("/signup", async (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
+
+
+});
 
 export default router;
