318086cd0e6a206d3679522cafc6b1d1863e62c3
Md Tasin Tahmid <md.tasin.tahmid1998@gmail.com>
2026-06-16 08:56:26 +0600

    Add sign up route handler

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,48 @@
 import express from "express";
 import db from "../db";
 import { usersTable } from "../models/user.model";
+import { eq } from "drizzle-orm";
+import { createHmac, randomBytes } from "crypto";
 
 const router = express.Router();
 
 router.post("/signup", async (req, res) => {
-  const { firstname, lastname, email, password } = req.body;
+	const { firstname, lastname, email, password } = req.body;
 
+	const [exitingUser] = db
+		.select({ id: usersTable.id })
+		.from(usersTable)
+		.where(eq(usersTable.email, email));
 
+	if (exitingUser)
+		return res
+			.status(400)
+			.json({ error: "User already exists with this email!" });
+
+	const salt = randomBytes(256).toString("hex");
+	const hashedPassword = createHmac("sha256", salt)
+		.update(password)
+		.digest("hex");
+
+	const [user] = await db
+		.insert(usersTable)
+		.values({
+			email,
+			firstname,
+			lastname,
+			salt,
+			password: hashedPassword,
+		})
+		.returning({
+			id: usersTable.id,
+		});
+
+	return res
+		.status(201)
+		.json({
+			data: { userId: user.id },
+			message: "User created successfully!",
+		});
 });
 
 export default router;
